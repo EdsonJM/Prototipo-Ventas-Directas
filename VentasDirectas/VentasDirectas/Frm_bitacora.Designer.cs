10342c64c9b1f4681384a9f38016d20aba56d155
Edson Juarez <edsonjm99@gmail.com>
2019-04-24 22:50:48 -0600

    Implementación de seguridad

---
+++ VentasDirectas/VentasDirectas/Frm_bitacora.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace VentasDirectas
+{
+    partial class Frm_bitacora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_bitacora));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Btn_eliminar = new System.Windows.Forms.Button();
+            this.Btn_minimizar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Txt_buscar = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.IdBitacora = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.operacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.usuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fechaproceso = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabla = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.CadetBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Btn_minimizar);
+            this.panel1.Controls.Add(this.Btn_eliminar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(950, 45);
+            this.panel1.TabIndex = 0;
+            // 
+            // Btn_eliminar
+            // 
+            this.Btn_eliminar.FlatAppearance.BorderSize = 0;
+            this.Btn_eliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_eliminar.Image = ((System.Drawing.Image)(resources.GetObject("Btn_eliminar.Image")));
+            this.Btn_eliminar.Location = new System.Drawing.Point(871, 5);
+            this.Btn_eliminar.Name = "Btn_eliminar";
+            this.Btn_eliminar.Size = new System.Drawing.Size(35, 35);
+            this.Btn_eliminar.TabIndex = 3;
+            this.Btn_eliminar.UseVisualStyleBackColor = true;
+            this.Btn_eliminar.Click += new System.EventHandler(this.Btn_eliminar_Click);
+            // 
+            // Btn_minimizar
+            // 
+            this.Btn_minimizar.FlatAppearance.BorderSize = 0;
+            this.Btn_minimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_minimizar.Image = ((System.Drawing.Image)(resources.GetObject("Btn_minimizar.Image")));
+            this.Btn_minimizar.Location = new System.Drawing.Point(795, 5);
+            this.Btn_minimizar.Name = "Btn_minimizar";
+            this.Btn_minimizar.Size = new System.Drawing.Size(35, 35);
+            this.Btn_minimizar.TabIndex = 4;
+            this.Btn_minimizar.UseVisualStyleBackColor = true;
+            this.Btn_minimizar.Click += new System.EventHandler(this.Btn_minimizar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "BITACORA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(70, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NOMBRE DE USUARIO:";
+            // 
+            // Txt_buscar
+            // 
+            this.Txt_buscar.Location = new System.Drawing.Point(276, 123);
+            this.Txt_buscar.Multiline = true;
+            this.Txt_buscar.Name = "Txt_buscar";
+            this.Txt_buscar.Size = new System.Drawing.Size(498, 22);
+            this.Txt_buscar.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(807, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(769, 504);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Actualizar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IdBitacora,
+            this.operacion,
+            this.usuario,
+            this.fechaproceso,
+            this.tabla});
+            this.dataGridView1.Location = new System.Drawing.Point(54, 157);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(852, 326);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // IdBitacora
+            // 
+            this.IdBitacora.HeaderText = "ID Bitacora";
+            this.IdBitacora.Name = "IdBitacora";
+            // 
+            // operacion
+            // 
+            this.operacion.HeaderText = "Operacion";
+            this.operacion.Name = "operacion";
+            // 
+            // usuario
+            // 
+            this.usuario.HeaderText = "Usuario";
+            this.usuario.Name = "usuario";
+            // 
+            // fechaproceso
+            // 
+            this.fechaproceso.HeaderText = "Fecha Proceso";
+            this.fechaproceso.Name = "fechaproceso";
+            // 
+            // tabla
+            // 
+            this.tabla.HeaderText = "Tabla afectada";
+            this.tabla.Name = "tabla";
+            // 
+            // Frm_bitacora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 550);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Txt_buscar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Frm_bitacora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frm_bitacora";
+            this.Load += new System.EventHandler(this.Frm_bitacora_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Btn_eliminar;
+        private System.Windows.Forms.Button Btn_minimizar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Txt_buscar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IdBitacora;
+        private System.Windows.Forms.DataGridViewTextBoxColumn operacion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn usuario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fechaproceso;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tabla;
+    }
+}
--- VentasDirectas/VentasDirectas/Frm_login.cs
+++ VentasDirectas/VentasDirectas/Frm_login.cs
@@ -7,14 +7,90 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Odbc;
 
 namespace VentasDirectas
 {
     public partial class Frm_login : Form
     {
+        public string tipoUsuario = "";
+        public string usuarioLogeado = "";
+        DateTime fecha = DateTime.Now;
+
         public Frm_login()
         {
             InitializeComponent();
+        }
+
+        private void Btn_eliminar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Btn_minimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void IniciarSesion()
+        {
+            try
+            {
+                string seleccionarUsuario = string.Format("SELECT * FROM tbl_usuarios;");
+                OdbcCommand comm = new OdbcCommand(seleccionarUsuario, Conexion.nuevaConexion());
+                OdbcDataReader mostrar = comm.ExecuteReader();
+
+                while(mostrar.Read())
+                {
+                    if((Txt_usuario.Text==mostrar.GetString(2)) && (Txt_contraseña.Text==mostrar.GetString(3)))
+                    {
+                        usuarioLogeado = mostrar.GetString(1); //iguala al valor del campo cuenta_usr
+                        tipoUsuario = mostrar.GetString(4); //asigna el valor del campo tipo_usuario
+                        MessageBox.Show("INICIO DE SESIÓN ACEPTADO");
+                        Frm_mdi mdiMenu = new Frm_mdi(usuarioLogeado,tipoUsuario);
+                        this.Hide();
+                        mdiMenu.Show();
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write("DATOS INCORRECTOS");
+                        Txt_usuario.Focus();
+                    }
+                }
+
+            }
+            catch(Exception err)
+            {
+                Console.Write("Error: " + err.Message);
+            }
+
+            try
+            {
+                OdbcCommand comm = new OdbcCommand("{call SP_InsertarBitacora(?,?,?,?)}", Conexion.nuevaConexion());
+                comm.CommandType = CommandType.StoredProcedure;
+                comm.Parameters.Add("ope", OdbcType.Text).Value = "INICIO DE SESIÓN";
+                comm.Parameters.Add("usr", OdbcType.Text).Value = usuarioLogeado;
+                comm.Parameters.Add("fecha", OdbcType.Text).Value = fecha.ToString("yyyy/MM/dd HH:mm:ss");
+                comm.Parameters.Add("tbl", OdbcType.Text).Value = "-----";
+                comm.ExecuteNonQuery();
+            }
+            catch(Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+
+        private void Btn_ingresar_Click(object sender, EventArgs e)
+        {
+            IniciarSesion();
+        }
+
+        private void Frm_login_Load(object sender, EventArgs e)
+        {
+            Txt_usuario.Focus();
+            usuarioLogeado = "";
+            
         }
     }
 }
